b6325deeb640c44e679075e6fc2769999561e182
bluevxhd <lionelrodriquez04@gmail.com>
2026-09-25 21:28:19 +0700

    Update title and header for student registration page

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Pendaftaran Siswa Baru | SMA Diakonia</title>
+    <title>Isi Biodata Siswa | SMA Diakonia</title>
 </head>
 
 <body>
     <header>
-        <h3>Pendaftaran Siswa Baru</h3>
+        <h3>Isi Biodata Siswa</h3>
         <h1>SMA Diakonia</h1>
     </header>
 
     <h4>Menu</h4>
     <nav>
         <ul>
-            <li><a href="form-daftar.php">Daftar Baru</a></li>
+            <li><a href="formdaftar.php">Daftar Baru</a></li>
             <li><a href="list-siswa.php">Pendaftar</a></li>
         </ul>
     </nav>
